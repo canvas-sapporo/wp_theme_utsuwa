0e4ee9e8a933d947ff244a9d91ae209fd55bc84d
ALMERIA-25\yonezawamasahiro <kaihatuadmin@almeria.jp>
2026-02-11 11:08:35 +0900

    Add Favicon

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
+
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="<?php echo get_template_directory_uri(); ?>/android-chrome-512x512.png">
+
+    <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
